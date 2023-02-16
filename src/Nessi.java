57803a5ed6f0b7f572ac2ad602c9b9959c094a0b
utente <utente@DESKTOP-I4VH3T8>
2023-02-16 09:23:11 +0100

    Ciao a tutti!

--- src/Nessi.java
+++ src/Nessi.java
@@ -3,36 +3,24 @@ import java.lang.Math;
 public class Nessi
 {
 	static Scanner in = new Scanner(System.in);
-	static double[]arrayone = new double[5];
-	static double[]arraytwo = new double[5];
-	static double[]arraythree = new double [10];
+	static double[] arrayone = new double[5];
+	static double[] arraytwo = new double[5];
+	static double[] arraythree = new double [10];
+	static double[]arrayfour  = new double [10];
 	public static void main(String[] args)
 	{
 		while(true)
 		{
-			for(int i = 0; i < arrayone.length; i++)
-			{
-				System.out.println("Inserire elemento num" + (i+1));
-				arrayone[i] = in.nextInt();
-			}
-			for(int i = 0; i < arraytwo.length; i++)
-			{
-				System.out.println("Inserire elemento num" + (i+1));
-				arraytwo[i] = in.nextInt();
-			}
 			stampaMenu();
 			int scelta = in.nextInt();
 			switch(scelta)
 			{
 				case 1:
-					es1(arrayone, arraytwo, arraythree);
+					es1();
 				break;
 				case 2:
-					for(int i = 0; i< arrayone.length; i++)
-					{
-						arrayone[i]=Math.random()*20-10;
-						
-					}
+					es2(arrayfour);
+
 					
 				break;
 				case 3:
@@ -48,13 +36,25 @@ public class Nessi
 	{
 		
 		System.out.println("1 - Es n. 1 - Titolo es.: Ordinamento dei valori di due array in ordine crescente, in un terzo array;");
-		System.out.println("2 - Es n. * - Titolo es. *");
+		System.out.println("2 - Es n. 2 - Titolo es.: Es2");
 		System.out.println("3 - Es n. * - Titolo es. *");
 		System.out.println("4 - Es n. * - Titolo es. *");
 		System.out.println("5 - Es n. * - Titolo es. *");
 	}
-	static void es1(double[]arrayone, double[]arraytwo, double[]arraythree)
+	static void es1()
 	{
+		for(int i = 0; i < arrayone.length; i++)
+		{
+			System.out.println("Inserire elemento num" + (i+1));
+			arrayone[i] = in.nextInt();
+		}
+		for(int i = 0; i < arraytwo.length; i++)
+		{
+			System.out.println("Inserire elemento num" + (i+1));
+			arraytwo[i] = in.nextInt();
+		}	
+		
+		
 		for(int i = 0; i < arrayone.length;i++)
 		{
 			arraythree[i] = arrayone[i];
@@ -85,21 +85,39 @@ public class Nessi
 		V[a] = V[b];
 		V[b] = temp;
 	}
-	static void es2(int[]a, int[]b)
+	static void es2(double[]a)
 	{
 		
+		for(int i = 0; i <arrayfour.length;i++)
+		{
+			arrayfour[i]=Math.random()*20-10;
+			System.out.println(arrayfour[i]);
+		}
+		double[]b=new double[10];
 		int contatore = -1;
 		for(int i = 0; i < a.length;i++)
 		{
 			
-			if(a[i]>= 0)
+			if(a[i]>= 0.000000000000000)
 			{
 				contatore += 1;
 				b[contatore]=a[i];
 			}
 		}
-		double[]banana = new double[b.length];
-		for(int i = 0, j = b.length;)
+		for(int i = 0; i < b.length;i++)
+		{
+			for(int j = 0; j < b.length;j++)
+			{
+				if(b[i]>b[j])
+				{
+					swap(i,j,b);
+				}
+			}
+		}
+		for(int i =0; i < contatore; i++)
+		{
+			System.out.println("Elementi maggiori uguali a 0 riordinati:" +b[i]);
+		}
 	}
 	
 }
